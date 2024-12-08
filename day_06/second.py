a604f3af5be6d2c4c0bbd49c5ece1e9bdecbf043
Gohér László <goher.laci@gmail.com>
2024-12-08 13:38:31 +0100

    day 06 part 2 somewhat faster solution (86s -> 52s) by not copying dicts

--- day_06/second.py
+++ day_06/second.py
@@ -1,26 +1,15 @@
 from sys import stdin
-from enum import Enum
 
 
 # coordinate system:
-#         -1j
-#          ^
-#          |
-#          |
-# -1 <---- 0 ----> +1
-#          |
-#          |
-#          v
-#         +1j
+#  0 ----> +1
+#  |
+#  |
+#  v
+# +1j
 
 
-class Cell(Enum):
-    EMPTY   = 0
-    OBST    = 1
-    VISITED = 2
-
-
-the_map = {}
+obstacles = set()
 the_guard = (0+0j, 0+0j) # (posittion, heading)
 width = 0
 height = 0
@@ -32,10 +21,8 @@ for row, line in enumerate(stdin):
         width = len(line.strip())
     for col, char in enumerate(line.strip()):
         match char:
-            case ".":
-                the_map[col+row*1j] = (Cell.EMPTY, None)
             case "#":      
-                the_map[col+row*1j] = (Cell.OBST, None)
+                obstacles.add(col+row*1j)
             case "^":      
                 the_guard = (col+row*1j, 0-1j)
             case "v":      
@@ -47,32 +34,36 @@ for row, line in enumerate(stdin):
 
 
 in_a_loop = 0
+
 for y in range(height):
     for x in range(width):
-        match the_map.get(x + y *1j):
-            case (Cell.EMPTY, _):
-                pass
-            case _:
-                continue
-        clean_map = the_map.copy()
-        clean_guard = the_guard
-        clean_map[x + y *1j] = (Cell.OBST, None)
+        point = x + y * 1j
+        if point in obstacles or point == the_guard[0]:
+            continue
+        
+        guard = the_guard
+        obstacles.add(point)
+        visited = {}
+
         while True:
-            position, heading = clean_guard
+            position, heading = guard
             if 0 < position.real < width and 0 < position.imag < height:
-                match clean_map.get(position):
-                    case (Cell.VISITED, h) if h == heading:
-                        in_a_loop += 1
-                        break
-                    case (Cell.EMPTY, _):
-                        clean_map[position] = (Cell.VISITED, heading)
+                is_visited = visited.get(position)
+                if is_visited == heading:
+                    in_a_loop += 1
+                    break
+                elif is_visited == None:
+                    visited[position] = heading
             else:
                 break
 
-            if clean_map.get(position + heading) == (Cell.OBST, None):
-                clean_guard = (position, heading * 1j)
+            next_point = position + heading
+            if next_point in obstacles:
+                guard = (position, heading * 1j)
             else:
-                clean_guard = (position + heading, heading)
+                guard = (next_point, heading)
+
+        obstacles.remove(point)
 
 
 print(in_a_loop)
