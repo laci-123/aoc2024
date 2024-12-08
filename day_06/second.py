bec4c3757542572a7798afbf984e94f264cd2107
Gohér László <goher.laci@gmail.com>
2024-12-08 14:20:30 +0100

    day 06 part 2 faster less brute force solution (52s -> 11s)
    Only put new obstacles along the original path
    that the guard followed in part 1.

--- day_06/second.py
+++ day_06/second.py
@@ -10,10 +10,9 @@ from sys import stdin
 
 
 obstacles = set()
-the_guard = (0+0j, 0+0j) # (posittion, heading)
+original_guard = (0+0j, 0+0j) # (posittion, heading)
 width = 0
 height = 0
-
 
 for row, line in enumerate(stdin):
     height += 1
@@ -24,46 +23,59 @@ for row, line in enumerate(stdin):
             case "#":      
                 obstacles.add(col+row*1j)
             case "^":      
-                the_guard = (col+row*1j, 0-1j)
+                original_guard = (col+row*1j, 0-1j)
             case "v":      
-                the_guard = (col+row*1j, 0+1j)
+                original_guard = (col+row*1j, 0+1j)
             case ">":      
-                the_guard = (col+row*1j, 1+0j)
+                original_guard = (col+row*1j, 1+0j)
             case "<":      
-                the_guard = (col+row*1j, -1+0j)
+                original_guard = (col+row*1j, -1+0j)
+
+
+visited_without_extra_obsts = set()
+guard = original_guard
+
+while True:
+    position, heading = guard
+    if 0 < position.real < width and 0 < position.imag < height:
+        if position != original_guard:
+            visited_without_extra_obsts.add(position)
+    else:
+        break
+
+    next_point = position + heading
+    if next_point in obstacles:
+        guard = (position, heading * 1j)
+    else:
+        guard = (next_point, heading)
 
 
 in_a_loop = 0
 
-for y in range(height):
-    for x in range(width):
-        point = x + y * 1j
-        if point in obstacles or point == the_guard[0]:
-            continue
-        
-        guard = the_guard
-        obstacles.add(point)
-        visited = {}
+for point in visited_without_extra_obsts:
+    guard = original_guard
+    obstacles.add(point)
+    visited = {}
 
-        while True:
-            position, heading = guard
-            if 0 < position.real < width and 0 < position.imag < height:
-                is_visited = visited.get(position)
-                if is_visited == heading:
-                    in_a_loop += 1
-                    break
-                elif is_visited == None:
-                    visited[position] = heading
-            else:
+    while True:
+        position, heading = guard
+        if 0 < position.real < width and 0 < position.imag < height:
+            is_visited = visited.get(position)
+            if is_visited == heading:
+                in_a_loop += 1
                 break
+            elif is_visited == None:
+                visited[position] = heading
+        else:
+            break
 
-            next_point = position + heading
-            if next_point in obstacles:
-                guard = (position, heading * 1j)
-            else:
-                guard = (next_point, heading)
+        next_point = position + heading
+        if next_point in obstacles:
+            guard = (position, heading * 1j)
+        else:
+            guard = (next_point, heading)
 
-        obstacles.remove(point)
+    obstacles.remove(point)
 
 
 print(in_a_loop)
