e37e11024d489c8a49cc2fc5fb1d609f3338aabd
Gohér László <goher.laci@gmail.com>
2024-12-08 13:58:27 +0100

    day 06 part 2 little bit faster solution (52s -> 43s) by not using complex numbers

--- day_06/second.py
+++ day_06/second.py
@@ -2,15 +2,37 @@ from sys import stdin
 
 
 # coordinate system:
-#  0 ----> +1
-#  |
-#  |
-#  v
-# +1j
+#   0 ----> +1 (x)
+#   |
+#   |
+#   v
+#  +1
+#  (y)
+
+
+UP    = (0, -1)
+RIGHT = (1, 0)
+DOWN  = (0, 1)
+LEFT  = (-1, 0)
+
+
+def rotate(heading):
+    if heading == UP:
+        return RIGHT
+    if heading == RIGHT:
+        return DOWN
+    if heading == DOWN:
+        return LEFT
+    if heading == LEFT:
+        return UP
+
+
+def move(position, heading):
+    return (position[0] + heading[0], position[1] + heading[1])
 
 
 obstacles = set()
-the_guard = (0+0j, 0+0j) # (posittion, heading)
+the_guard = ((0, 0), (0, 0)) # (posittion, heading)
 width = 0
 height = 0
 
@@ -22,22 +44,22 @@ for row, line in enumerate(stdin):
     for col, char in enumerate(line.strip()):
         match char:
             case "#":      
-                obstacles.add(col+row*1j)
+                obstacles.add((col, row))
             case "^":      
-                the_guard = (col+row*1j, 0-1j)
+                the_guard = ((col, row), UP)
             case "v":      
-                the_guard = (col+row*1j, 0+1j)
+                the_guard = ((col, row), DOWN)
             case ">":      
-                the_guard = (col+row*1j, 1+0j)
+                the_guard = ((col, row), RIGHT)
             case "<":      
-                the_guard = (col+row*1j, -1+0j)
+                the_guard = ((col, row), LEFT)
 
 
 in_a_loop = 0
 
 for y in range(height):
     for x in range(width):
-        point = x + y * 1j
+        point = (x, y)
         if point in obstacles or point == the_guard[0]:
             continue
         
@@ -47,7 +69,7 @@ for y in range(height):
 
         while True:
             position, heading = guard
-            if 0 < position.real < width and 0 < position.imag < height:
+            if 0 < position[0] < width and 0 < position[1] < height:
                 is_visited = visited.get(position)
                 if is_visited == heading:
                     in_a_loop += 1
@@ -57,9 +79,9 @@ for y in range(height):
             else:
                 break
 
-            next_point = position + heading
+            next_point = move(position, heading)
             if next_point in obstacles:
-                guard = (position, heading * 1j)
+                guard = (position, rotate(heading))
             else:
                 guard = (next_point, heading)
 
