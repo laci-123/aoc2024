10fa5d6e29fd41aed45630054fa9a34e94d5c0b5
Gohér László <goher.laci@gmail.com>
2024-12-09 15:12:27 +0100

    day 09 second part even slower but correct solution

--- day_09/second.py
+++ day_09/second.py
@@ -1,88 +1,122 @@
 from sys import stdin
 
 
+class Node:
+    def __init__(self, value, prev, next):
+        self.value = value
+        self.prev = prev
+        self.next = next
+
+    def insert_after(self, value):
+        p = self.next
+        n = Node(value, self, self.next)
+        self.next = n
+        if p != None:
+            p.prev = n
+        return n
+
+    def delete_next(self):
+        if self.next:
+            p = self.next.next
+            self.next = p
+            if p:
+                p.prev = self
+
+
+class LinkedList:
+    def __init__(self):
+        self.first = None
+        self.last = None
+
+    def append(self, value):
+        if self.last:
+            self.last = self.last.insert_after(value)
+        else:
+            self.first = Node(value, None, None)
+            self.last = self.first
+
+
+class Block:
+    def __init__(self, id, length):
+        self.id = id
+        self.length = length
+
+
 def decompress(disk):
     is_file = True
     id = 0
-    result = []
+    result = LinkedList()
     for x in disk:
         if is_file:
-            result += [id] * x
+            result.append(Block(id, x))
             id += 1
         else:
-            result += [-1] * x
+            result.append(Block(-1, x))
         is_file = not is_file
     return result
 
 
-def find_nth_last_file(disk, n):
-    k = len(disk) - 1
-    id = -1
-    length = -1
-    for _ in range(n):
-        while disk[k] == -1:
-            k -= 1
-        id = disk[k]
-        length = 0
-        if k < 0:
-            return None
-        while disk[k] == id:
-            k -= 1
-            length += 1
-    return (k + 1, id, length)
-
-
-def find_first_big_enough_free(disk, target_length):
-    length = 0
-    k = -1
-    for i, id in enumerate(disk):
-        if id == -1:
-            if k == -1:
-                k = i
-            length += 1
-        elif k != -1:
-            if length >= target_length:
-                return (k, length)
-            else:
-                length = 0
-                k = -1
-
-
 def defragment(disk):
-    n = 0
-    while True:
-        match find_nth_last_file(disk, n):
-            case k, id, len_file:
-                i, len_free     = find_first_big_enough_free(disk, len_file)
-                if i < k and len_file <= len_free:
-                    disk[i:(i + len_file)] = [id] * len_file
-                    disk[k:(k + len_file)] = [-1] * len_file
-                n += 1
-            case _:
+    file = disk.last
+    while file:
+        if file.value.id == -1:
+            file = file.prev
+        free = disk.first
+        ids = []
+        while free:
+            ids.append(free.value.id)
+            while free.value.id != -1:
+                free = free.next
+                ids.append(free.value.id)
+            if file.value.length <= free.value.length and file.value.id not in ids:
+                rem_length = free.value.length - file.value.length
+                free.value.id = file.value.id
+                free.value.length = file.value.length
+                if rem_length > 0:
+                    free.insert_after(Block(-1, rem_length))
+                file.value.id = -1
+                if file.next and file.next.value.id == -1:
+                    file.value.length += file.next.value.length
+                    file.delete_next()
+                if file.prev and file.prev.value.id == -1:
+                    file.prev.value.length += file.value.length
+                    file.prev.delete_next()
                 break
+            free = free.next
+        file = file.prev
 
 
-# def pretty_print(disk):
-#     s = ""
-#     for x in disk:
-#         if x == -1:
-#             s += "."
-#         else:
-#             s += str(x)
-#     return s
+def pretty_print(disk):
+    s = ""
+    node = disk.first
+    while node:
+        if node.value.id == -1:
+            s += "." * node.value.length
+        else:
+            s += str(node.value.id) * node.value.length
+        node = node.next
+    return s
 
 
 def checksum(disk):
     sum = 0
-    for i, x in enumerate(disk):
-        if x == -1:
-            continue
-        sum += i * x
+    node = disk.first
+    i = 0
+    while node:
+        if node.value.id != -1:
+            for _ in range(node.value.length):
+                sum += i * node.value.id 
+                i += 1
+        else:
+            i += node.value.length
+        node = node.next
     return sum
             
 
 
 line = [int(x) for x in stdin.read().strip()]
 disk = decompress(line)
+# print(pretty_print(disk))
 defragment(disk)
+# print(pretty_print(disk))
 print(checksum(disk))
